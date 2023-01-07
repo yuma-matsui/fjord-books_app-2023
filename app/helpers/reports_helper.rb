e98edafb1590a78e7e5a5a97af8361c86ada06d5
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-07 22:23:01 +0900

    日報に別の日報のurlが含まれている場合リンクで表示するようにした

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,4 +8,11 @@ module ReportsHelper
   def mentioned_reports(report)
     report.mentioned_reports.latest_order
   end
+
+  def format_mention(text)
+    text.gsub(Report::LINK_MATCHER) do |url|
+      target = Report.find_by(id: ::Regexp.last_match(1))
+      target.nil? ? url : "<a href='#{url}'>#{url}</a>"
+    end
+  end
 end
