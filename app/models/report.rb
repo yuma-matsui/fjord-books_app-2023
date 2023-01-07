c7ee37487642d07e3faf4a379250f724f58e3f45
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-07 09:28:52 +0900

    日報内に含まれている日報のリンクを返すメソッドを用意した

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
+  LINK_MATCHER = %r{http://localhost:3000/reports/([1-9]+\d*)}
+
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
@@ -9,6 +11,17 @@ class Report < ApplicationRecord
 
   def editable?(target_user)
     user == target_user
+  end
+
+  def including_mention?
+    content.match?(LINK_MATCHER)
+  end
+
+  def mention
+    content.scan(LINK_MATCHER).flatten.each_with_object([]) do |report_id, reports|
+      report = Report.find_by(id: report_id)
+      reports.push(report) unless reports.include?(report) || report.nil?
+    end
   end
 
   def created_on
