3af8582711268b210a35c7a327d6c33e0e2178e1
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-07 10:08:30 +0900

    日報のメンションの関連付けとメンションを追加/削除するメソッドを用意した

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,6 +6,18 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_mentioning, class_name: 'MentioningMentionedReport',
+                               foreign_key: 'mentioned_report_id',
+                               inverse_of: :mentioned_report,
+                               dependent: :destroy
+  has_many :mentioning_reports, through: :active_mentioning, source: :mentioning_report
+
+  has_many :passive_mentioning, class_name: 'MentioningMentionedReport',
+                                foreign_key: 'mentioning_report_id',
+                                inverse_of: :mentioning_report,
+                                dependent: :destroy
+  has_many :mentioned_reports, through: :passive_mentioning, source: :mentioned_report
+
   validates :title, presence: true
   validates :content, presence: true
 
@@ -22,6 +34,14 @@ class Report < ApplicationRecord
       report = Report.find_by(id: report_id)
       reports.push(report) unless reports.include?(report) || report.nil?
     end
+  end
+
+  def add_mention(report)
+    mentioning_reports << report
+  end
+
+  def remove_mention(report)
+    active_mentioning.find_by(mentioning_report_id: report.id).destroy!
   end
 
   def created_on
