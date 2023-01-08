be08939a14da0ac154513dd97b3b6aa1b314b5cb
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-08 15:52:41 +0900

    不要なヘルパーメソッドを削除した && presenceをexists?に変更した

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module ReportsHelper
-  def mentioned?(report)
-    report.mentioned_reports.presence
-  end
-
-  def mentioned_reports(report)
-    report.mentioned_reports.latest_order
-  end
-
   def format_mention(text)
     text.gsub(Report::LINK_MATCHER) do |url|
       target = Report.find_by(id: Regexp.last_match(1))
