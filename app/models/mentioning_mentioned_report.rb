7a0df023f1dd2f02ae171085fff5781958c1895f
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-09 13:43:57 +0900

    重複していたヴァリデーションの記述を削除した

--- app/models/mentioning_mentioned_report.rb
+++ app/models/mentioning_mentioned_report.rb
@@ -4,5 +4,4 @@ class MentioningMentionedReport < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
   validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
-  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
 end
